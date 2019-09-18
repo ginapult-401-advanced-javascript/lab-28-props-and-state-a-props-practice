a34a753ba7deb4d77a50c392c210ab4d97c244d5
Gina Pultorak Scarpello <50841963+ginapult@users.noreply.github.com>
2019-09-18 13:56:32 -0700

    lab-28-a-part1

--- src/index.js
+++ src/index.js
@@ -5,9 +5,33 @@ import './styles.css';
 
 class App extends React.Component {
   render() {
-    return <div />;
+    return (
+      <React.Fragment>
+        <Message />
+      </React.Fragment>
+    );
+  }
+}
+
+class Message extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Hello World',
+    };
+  }
+  
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <p>{this.state.text}</p>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
+export default Message;
